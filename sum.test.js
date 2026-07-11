dabd1139127e2b7e96993ce18f4cc468c4ad2d1a
Pollygon <pollygondev@gmail.com>
2026-07-11 15:47:55 +0800

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -2,6 +2,6 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
 expect(sum(1, 2)).toBe(3);
 });
-test('adds 5 + 7 to equal 12', () => {
-expect(sum(5, 7)).toBe(12);
+test('adds 5 + 10 to equal 15', () => {
+expect(sum(5, 10)).toBe(15);
 });
